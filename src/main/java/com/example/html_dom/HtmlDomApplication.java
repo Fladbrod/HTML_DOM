4c0326c71e21c031e7964a06a98f500e988226e5
Fladbrod <73465595+Fladbrod@users.noreply.github.com>
2021-11-01 11:18:09 +0100

    Classes

---
+++ src/main/java/com/example/html_dom/HtmlDomApplication.java
@@ -0,0 +1,13 @@
+package com.example.html_dom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlDomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlDomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/html_dom/HtmlDomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.html_dom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlDomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
